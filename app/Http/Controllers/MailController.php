e0ead1b80ae8a8a8cbb56fb86d2100483183f163
Sojan TS <64725062+sojan-ts@users.noreply.github.com>
2023-01-25 13:18:31 +0530

    Update MailController.php

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -10,9 +10,9 @@ class MailController extends Controller
     public function sendmail() {
     $data = array('name'=>"Sample Name");
     Mail::send('mail', $data, function($message) {
-        $message->to('sojants7@gmail.com', 'SojanTS')->subject
+        $message->to('tomail@gmail.com', 'SojanTS')->subject
         ('Lumen Basic Testing Mail');
-        $message->from('developer.tisser@gmail.com','Dude');
+        $message->from('yourfrom@gmail.com','Dude');
     });
     echo "Basic Email Sent. Check your inbox.";
 }
